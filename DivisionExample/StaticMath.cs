8f6a6e48f5aceefc310b29eff8f8052e509cffc1
bradleysundberg <149118782+bradleysundberg@users.noreply.github.com>
2025-11-19 12:33:26 -0800

    Add files via upload

---
+++ DivisionExample/StaticMath.cs
@@ -0,0 +1,10 @@
+﻿namespace DivisionApp // Namespace for the static class
+{
+    static class StaticMath // Declares a static class
+    {
+        public static int AddTen(int number) // Static method that adds ten to a number
+        {
+            return number + 10; // Returns the new value
+        }
+    }
+}
