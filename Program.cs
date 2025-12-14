bb4dfda515d9f482ab3f6c51cc4be5e599d1fa1d
bradleysundberg <149118782+bradleysundberg@users.noreply.github.com>
2025-12-14 11:03:48 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CodeFirstNewDatabaseSample
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (StudentContext context = new StudentContext())
+            {
+                context.Database.EnsureCreated();
+
+                Student student = new Student
+                {
+                    FirstName = "John",
+                    LastName = "Doe"
+                };
+
+                context.Students.Add(student);
+                context.SaveChanges();
+
+                Console.WriteLine("Student added successfully.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
