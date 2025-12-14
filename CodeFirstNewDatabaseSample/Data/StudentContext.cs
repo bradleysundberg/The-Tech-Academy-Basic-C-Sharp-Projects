2f571e2758d520681a611e7aad8920fa5138296e
bradleysundberg <149118782+bradleysundberg@users.noreply.github.com>
2025-12-14 11:04:46 -0800

    Add files via upload

---
+++ CodeFirstNewDatabaseSample/Data/StudentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstNewDatabaseSample
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                "Server=(localdb)\\MSSQLLocalDB;Database=StudentDatabase;Trusted_Connection=True;");
+        }
+    }
+}
